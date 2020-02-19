bcf8de8270c45b0c3e72e848e6bc76743a43697a
joelmelvin2805 <joelmelvin.johnbritto@cognizant.com>
2020-02-19 17:33:27 -0500

    Config app

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,14 +1,22 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String displayMessage;
+
+    public WelcomeController( @Value("${welcome.message}") String displayMessage)
+    {
+        this.displayMessage = displayMessage;
+    }
+
     @GetMapping("/")
     public String sayHello(){
-        return("Hello");
+        return(displayMessage);
     }
 
 }
